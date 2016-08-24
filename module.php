b5e64f8039719a40a98d0145c9ce4fb20eaf473c
nadine-shi <nadine@afterlogic.com>
2016-08-24 14:43:51 +0300

    System - create checkUserRoleIsAtLeast method in Api

--- module.php
+++ module.php
@@ -4,6 +4,8 @@ class ChangePasswordWebclientModule extends AApiModule
 {
 	public function GetAppData()
 	{
+		\CApi::checkUserRoleIsAtLeast(\EUserRole::Anonymous);
+		
 		return array(
 			'PasswordMinLength' => 0, // AppData.App.PasswordMinLength
 			'PasswordMustBeComplex' => false // AppData.App.PasswordMustBeComplex
