61fb392feb2dd30d91b20df0ba3b8dc3ac88a63a
nadine-shi <nadine@afterlogic.com>
2016-09-13 17:22:36 +0300

    System - use manager.js files instead of manager-smth.js

--- js/manager.js
+++ js/manager.js
@@ -1,23 +1,26 @@
 'use strict';
 
 module.exports = function (oAppData) {
+	var App = require('%PathToCoreWebclientModule%/js/App.js');
+	
+	if (App.getUserRole() === Enums.UserRole.NormalUser)
+	{
+		var
+			Settings = require('modules/%ModuleName%/js/Settings.js'),
+			oSettings = oAppData['%ModuleName%'] || {}
+		;
 
-	var
-		Settings = require('modules/%ModuleName%/js/Settings.js'),
-		oSettings = oAppData['%ModuleName%'] || {}
-	;
+		Settings.init(oSettings);
+
+		return {
+			getChangePasswordPopup: function () {
+				return require('modules/%ModuleName%/js/popups/ChangePasswordPopup.js');
+			},
+			getResetPasswordView: function () {
+				return require('modules/%ModuleName%/js/views/ResetPasswordView.js');
+			}
+		};
+	}
 	
-	Settings.init(oSettings);
-	
-	return {
-		isAvailable: function (iUserRole, bPublic) {
-			return !bPublic && iUserRole === Enums.UserRole.NormalUser;
-		},
-		getChangePasswordPopup: function () {
-			return require('modules/%ModuleName%/js/popups/ChangePasswordPopup.js');
-		},
-		getResetPasswordView: function () {
-			return require('modules/%ModuleName%/js/views/ResetPasswordView.js');
-		}
-	};
+	return null;
 };
