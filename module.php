fe3625dc40dee57d82f4dbefe144e6e9579fb669
sash04ek <a.ovcharov@gmail.com>
2017-02-28 10:50:53 +0300

    global api refactoring

--- module.php
+++ module.php
@@ -2,7 +2,7 @@
 
 namespace Aurora\Modules;
 
-class ChangePasswordWebclientModule extends \AApiModule
+class ChangePasswordWebclientModule extends \Aurora\System\AbstractModule
 {
 	/***** public functions might be called with web API *****/
 	/**
@@ -12,7 +12,7 @@ class ChangePasswordWebclientModule extends \AApiModule
 	 */
 	public function GetSettings()
 	{
-		\CApi::checkUserRoleIsAtLeast(\EUserRole::Anonymous);
+		\Aurora\System\Api::checkUserRoleIsAtLeast(\EUserRole::Anonymous);
 		
 		return array(
 			'PasswordMinLength' => 0, // AppData.App.PasswordMinLength
