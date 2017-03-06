f84aed6f07bc0236dd8bd123082b9d534943856c
sash04ek <a.ovcharov@gmail.com>
2017-03-06 08:44:27 +0300

    refactoring namespaces

--- module.php
+++ module.php
@@ -1,8 +1,8 @@
 <?php
 
-namespace Aurora\Modules;
+namespace Aurora\Modules\ChangePasswordWebclient;
 
-class ChangePasswordWebclientModule extends \Aurora\System\Module\AbstractModule
+class Module extends \Aurora\System\Module\AbstractModule
 {
 	/***** public functions might be called with web API *****/
 	/**
