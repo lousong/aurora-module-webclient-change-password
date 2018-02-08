dfc5ea32fe0e08ff524f6a5fef27e7cf4c64de49
nadine-shi <nadine@afterlogic.com>
2018-02-08 16:55:50 +0300

    ModulesManager.run result should be checked before use

--- js/views/ChangeSingleMailAccountPasswordView.js
+++ js/views/ChangeSingleMailAccountPasswordView.js
@@ -1,6 +1,7 @@
 'use strict';
 
 var
+	_ = require('underscore'),
 	ko = require('knockout'),
 	
 	ModulesManager = require('%PathToCoreWebclientModule%/js/ModulesManager.js'),
@@ -26,7 +27,7 @@ CChangeSingleMailAccountPasswordView.prototype.ViewTemplate = '%ModuleName%_Chan
 CChangeSingleMailAccountPasswordView.prototype.init = function ()
 {
 	var oAccountList = ModulesManager.run('MailWebclient', 'getAccountList', []);
-	if (oAccountList.collection().length === 1 && !Settings.MailAllowAddAccounts)
+	if (oAccountList && _.isFunction(oAccountList.collection) && oAccountList.collection().length === 1 && !Settings.MailAllowAddAccounts)
 	{
 		this.oSingleMailAccount = oAccountList.collection()[0];
 		this.showChangePasswordButton(true);
