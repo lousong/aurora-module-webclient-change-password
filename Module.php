a5059528f987a639c7b28b8d381bd974ae603932
sash04ek <a.ovcharov@gmail.com>
2018-11-19 16:16:19 +0300

    global refactoring

--- Module.php
+++ Module.php
@@ -14,7 +14,7 @@ namespace Aurora\Modules\ChangePasswordWebclient;
  *
  * @package Modules
  */
-class Module extends \Aurora\System\Module\AbstractModule
+class Module extends \Aurora\System\Module\AbstractWebclientModule
 {
 	/***** public functions might be called with web API *****/
 	/**
